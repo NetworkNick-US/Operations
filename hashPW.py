92287387ca7eabef79837b195810b2ff7eebfe45
NetworkNick <nikwilliams54@gmail.com>
2022-02-01 23:42:35 -0500

    Fix string formatting

--- hashPW.py
+++ hashPW.py
@@ -23,7 +23,7 @@ def hashPass(salted,pwd):
 try:
     os.system("")
     print("This script will help you hash a password for use with your Ansible playbooks for IOS and IOS XE devices.\n"
-        ,style.RED, "PLEASE NOTE: CURRENTLY,THE NXOS_USER ANSIBLE MODULE REQUIRES CLEAR-TEXT PASSWORDS", style.RESET)
+        + style.RED + "PLEASE NOTE: CURRENTLY,THE NXOS_USER ANSIBLE MODULE REQUIRES CLEAR-TEXT PASSWORDS", style.RESET)
     salt = getpass.getpass(prompt="Please enter a random string as your salt: ", stream=None)
     userpasswd = getpass.getpass(prompt="Password: ", stream=None)
     print("The value you should be using for your variable 'fallbackAdminPW' is: " + hashPass(salt,userpasswd))
