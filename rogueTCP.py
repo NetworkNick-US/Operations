f09fcf319896544119d50279d2ecdcb3eb31ad27
Network Nick <75176169+NetworkNick-US@users.noreply.github.com>
2022-02-02 20:39:30 -0500

    Update rogueTCP.py

--- rogueTCP.py
+++ rogueTCP.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 
+#usually written to a file and searched with grep or related tool
 While True:
     check = subprocess.getoutput("lsof - nPi tcp")
     print(str(check))
