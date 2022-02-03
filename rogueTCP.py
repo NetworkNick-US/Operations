2560b69b6a4a8d88a86ea5e705be120693ed9a42
NetworkNick <nikwilliams54@gmail.com>
2022-02-02 20:44:53 -0500

    Change loop structure

--- rogueTCP.py
+++ rogueTCP.py
@@ -4,7 +4,11 @@ import time
 import subprocess
 
 #usually written to a file and searched with grep or related tool
-While True:
-    check = subprocess.getoutput("lsof - nPi tcp")
-    print(str(check))
-    time.sleep(1)
+try:
+    While True:
+        check = subprocess.getoutput("lsof - nPi tcp")
+        print(str(check))
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("\n\nKeyboardInterrupt. Exiting script.... (╯°□°)╯︵ ┻━┻\n\n")
